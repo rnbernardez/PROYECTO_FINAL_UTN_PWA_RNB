60a9ca157fb39bc70ebd9743149e3e7d7a833b2e
rnbernardez <rnbernardez93@gmail.com>
2025-03-25 15:51:06 -0300

    Deploy 0.3

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './context/cartContext.js';
+import { CartProvider } from './context/cartContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
