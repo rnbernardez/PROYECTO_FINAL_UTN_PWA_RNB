6774d1393ae16f7fd2cf347bdba83435033f2966
rnbernardez <rnbernardez93@gmail.com>
2025-03-25 16:41:11 -0300

    Added alias con viteconfig

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from './context/CartContext';
+import { CartProvider } from '@/context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
