a2b854e4d520b831fc6faa8e0b3f651226e46d11
rnbernardez <rnbernardez93@gmail.com>
2025-03-25 16:35:48 -0300

    0.6 - CartContext import problems persist

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from '../src/context/CartContext.jsx';
+import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
