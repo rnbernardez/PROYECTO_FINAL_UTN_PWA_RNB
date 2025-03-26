031393db795aebd7ce19dc894aaf8271187b879c
rnbernardez <rnbernardez93@gmail.com>
2025-03-26 11:16:25 -0300

    Final deploy testing 1.0

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
       '@': '/src', // Usamos '@' como alias para la carpeta 'src'
     },
   },
+  server: {
+    port: process.env.PORT || 4173, // Usa el puerto de Render o el 4173 por defecto
+    host: true, // Esto permite que la app esté accesible en la red
+  },
 });
