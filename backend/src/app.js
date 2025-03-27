83f222dd891edcdbea65b18a415542ad00f66fab
rnbernardez <rnbernardez93@gmail.com>
2025-03-27 19:56:45 -0300

    Auth en routes faILED

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,6 @@ import shopRouter from './routes/shopRouter.js';
 import cartRouter from './routes/cartRouter.js';
 import connectToMongoDB from './config/MongoDBconfig.js';
 import dotenv from 'dotenv';
-import { authMiddleware } from './middlewares/authMiddleware.js';
 
 dotenv.config();
 connectToMongoDB();
@@ -17,7 +16,7 @@ app.use(express.json());
 
 app.use('/api/user', userRouter);
 app.use('/api/shop', shopRouter);
-app.use('/api/cart', authMiddleware, cartRouter);
+app.use('/api/cart', cartRouter);
 
 app.listen(ENVIROMENT.PORT, () => {
   console.log(`Server ejecutandose en el puerto http://localhost:${ENVIROMENT.PORT}`);
