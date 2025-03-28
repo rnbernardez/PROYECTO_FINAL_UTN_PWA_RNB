82fcc0755d83e350186a9a8e7d5939bf691c2f49
rnbernardez <rnbernardez93@gmail.com>
2025-03-27 21:13:00 -0300

    main.jsx problem cartContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { CartProvider } from '@/context/cartContext.jsx';
+import { CartProvider } from './context/cartContext.jsx';
 import './styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
