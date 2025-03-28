f74ff4f49a787a0b8f9e6a949bc9ada4ee0062ca
rnbernardez <rnbernardez93@gmail.com>
2025-03-27 22:36:27 -0300

    Backend cart modifs.

--- backend/src/routes/cartRouter.js
+++ backend/src/routes/cartRouter.js
@@ -11,12 +11,13 @@ import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const cartRouter = express.Router();
 
-cartRouter.get("/cart", authMiddleware, cartController);
+// Rutas modificadas para que coincidan con la estructura /api/cart
+cartRouter.get("/", authMiddleware, cartController); // Cambiado de "/cart" a "/"
 cartRouter.post("/add-card", authMiddleware, addCardController);
 cartRouter.get("/checkout", authMiddleware, checkoutController);
 cartRouter.post("/purchaseok", authMiddleware, purchaseOkController);
-cartRouter.post("/cart/add-product", authMiddleware, addProductToCartController);
-cartRouter.delete("/cart/remove-product/:productId", authMiddleware, removeProductFromCartController);
-cartRouter.delete("/cart/clear", authMiddleware, clearCartController);
+cartRouter.post("/add-product", authMiddleware, addProductToCartController); // Cambiado de "/cart/add-product"
+cartRouter.delete("/remove-product/:productId", authMiddleware, removeProductFromCartController); // Cambiado de "/cart/remove-product"
+cartRouter.delete("/clear", authMiddleware, clearCartController); // Cambiado de "/cart/clear"
 
 export default cartRouter;
