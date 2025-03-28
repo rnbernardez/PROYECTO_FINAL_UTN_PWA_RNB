9dd9bcf390ecd1affb55d71fbcca2a0331517437
rnbernardez <rnbernardez93@gmail.com>
2025-03-27 21:16:24 -0300

    main.jsx problems

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/cartContext.jsx';
 import './styles/main.css';
